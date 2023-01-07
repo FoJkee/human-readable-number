59882995c96121bb9ba2b9525421740a7eb87a58
ViktorRomanovsky <Romanovsky0815@gmail.com>
2023-01-07 22:20:52 +0300

    fix: mistake

--- src/index.js
+++ src/index.js
@@ -1,75 +1,75 @@
 module.exports = function toReadable(number) {
-    const ones = (num) => {
-        switch (num) {
-            case 1:
-                return "one";
-            case 2:
-                return "two";
-            case 3:
-                return "three";
-            case 4:
-                return "four";
-            case 5:
-                return "five";
-            case 6:
-                return "six";
-            case 7:
-                return "seven";
-            case 8:
-                return "eight";
-            case 9:
-                return "nine";
-            case 10:
-                return "ten";
-            case 11:
-                return "eleven";
-            case 12:
-                return "twelve";
-            case 13:
-                return "thirteen";
-            case 14:
-                return "fourteen";
-            case 15:
-                return "fifteen";
-            case 16:
-                return "sixteen";
-            case 17:
-                return "seventeen";
-            case 18:
-                return "eighteen";
-            case 19:
-                return "nineteen";
-        }
+    const ones = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
     };
-    const tens = (num) => {
-        switch (num) {
-            case 2:
-                return "twenty";
-            case 3:
-                return "thirty";
-            case 4:
-                return "forty";
-            case 5:
-                return "fifty";
-            case 6:
-                return "sixty";
-            case 7:
-                return "seventy";
-            case 8:
-                return "eighty";
-            case 9:
-                return "ninety";
-        }
+    const teens = {
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
     };
+
+    const tens = {
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+    };
+
     let a = String(number).split("");
-
-    if (number < 20) {
-        return ones(a);
-    }
-
-    if (number === 0) {
+    if (number == 0) {
         return "zero";
     }
 
-    return result;
+    if (number < 10) {
+        return ones[number];
+    } else if (number < 20) {
+        return teens[number];
+    }
+    // 19 - 99
+    if (a.length === 2) {
+        if (number > 19) {
+            if (+a[1] == 0) {
+                return `${tens[a[0] * 10]}`;
+            } else {
+                return `${tens[a[0] * 10]} ${ones[a[1]]}`;
+            }
+        }
+    }
+
+    // 100 - 999
+
+    if (a.length === 3) {
+        if (number > 99) {
+            if (+a[1] == 0 && +a[2] == 0) {
+                return `${ones[a[0]]} hundred`;
+            } else if (+a[1] == 0 && +a[2] != 0) {
+                return `${ones[a[0]]} hundred ${ones[a[2]]}`;
+            } else if (+a[1] == 1) {
+                return `${ones[a[0]]} hundred ${teens[a[1] + a[2]]}`;
+            } else if (+a[1] > 1 && +a[2] == 0) {
+                return `${ones[a[0]]} hundred ${tens[a[1] + a[2]]}`;
+            } else {
+                return `${ones[a[0]]} hundred ${tens[a[1] * 10]} ${ones[a[2]]}`;
+            }
+        }
+    }
 };
